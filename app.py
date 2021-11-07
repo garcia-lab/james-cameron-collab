e62cc949a8b72674ed211f9377427b8ab9af78f0
James Garcia <colossus9@github.com>
2021-11-06 21:30:43 -0500

    Adding comments

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+# This is a comment. This program adds two numbers.
+
+# This function does the math
 def addTwoNumbers(num1, num2):
 	answer = num1 + num2
 	print(answer)
 
+# This calls the functions
 def main():
 	addTwoNumbers(3, 5)
 
+# This is the main entry point
 if __name__ == "__main__":
     main()
