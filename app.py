78f70b78cb2ce095236272ca0239c500dc458952
James Garcia <colossus9@github.com>
2021-11-06 22:08:10 -0500

    Renamed the function

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # This is a comment. This program adds two numbers.
 
 # This function does the math
-def sumOfTwoNumbers(num1, num2):
+def addingNumbers(num1, num2):
 	answer = num1 + num2
 	showMeTheOutput(answer)
 
@@ -10,7 +10,7 @@ def showMeTheOutput(theSomething):
 
 # This calls the functions
 def main():
-	sumOfTwoNumbers(4, 6)
+	addingNumbers(4, 6)
 
 # This is the main entry point
 if __name__ == "__main__":
