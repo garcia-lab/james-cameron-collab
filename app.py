fdd960103c6a5d2e2313dac87ec66da5304b3507
James Garcia <colossus9@github.com>
2021-11-06 21:37:04 -0500

    Changing function name and numbers

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # This is a comment. This program adds two numbers.
 
 # This function does the math
-def addTwoNumbers(num1, num2):
+def sumOfTwoNumbers(num1, num2):
 	answer = num1 + num2
 	print(answer)
 
 # This calls the functions
 def main():
-	addTwoNumbers(3, 5)
+	sumOfTwoNumbers(4, 6)
 
 # This is the main entry point
 if __name__ == "__main__":
